2f18331ca3e93785e59e001ae836fe33bb60bce3
klavsss <a230441kv@rvt.lv>
2026-05-16 16:59:10 +0300

    Pievienots CORS middleware

--- my-laravel-app/bootstrap/app.php
+++ my-laravel-app/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->prepend(\App\Http\Middleware\Cors::class);
         $middleware->alias([
             'auth.token' => \App\Http\Middleware\CheckApiToken::class,
         ]);
